796bb76bea7f552b32b8428d4eb31bf13a5503dc
Rachel Cheman <rachelcheman@gmail.com>
2023-12-04 01:37:59 -0500

    Day4 part 2

--- Day4/2023_4.js
+++ Day4/2023_4.js
@@ -5,35 +5,50 @@ const data = fs.readFileSync(path.resolve(__dirname, './2023_4_data.txt'), 'utf8
 
 const cards = data.split("\n")
 
-let total = 0;
+let totalCards = 0;
+
+let cardCopies = {}
 
 for (let i = 0; i < cards.length; i++) {
-  let cardPoints = 0;
+  let cardNumberArray = (cards[i].split(" | ")[0].split(": ")[0].split(" "))
+  let cardNumber = Number(cardNumberArray[cardNumberArray.length - 1])
   let winningNums = cards[i].split(" | ")[0].split(": ")[1].split(" ")
   // single digit numbers have a space in front of them so we end up with an empty string in the array before a single digit number
   // In the sample there are 5 winning numbers, in the full data set there are 10
   let myNums = cards[i].split(" | ")[1].split(" ")
 
-  // loop through my numbers and see if any of them are winning numbers
-  for (let j = 0; j < myNums.length; j++) {
-    if (myNums[j] === ''){
-      continue
-    }
+  if (!cardCopies[cardNumber]) {
+    cardCopies[cardNumber] = 1;
+  } else {
+    cardCopies[cardNumber]++;
+  }
 
-    if (winningNums.includes(myNums[j])){
-      if (cardPoints === 0) {
-        cardPoints ++
-      } else {
-        cardPoints *= 2
+  // Check for card copies and loop through the number of copies you have plus one to account for original
+  for (let k = cardCopies[cardNumber]; k > 0; k --){
+    let cardWins = 0;
+    // loop through my numbers and see if any of them are winning numbers
+    for (let j = 0; j < myNums.length; j++) {
+      if (myNums[j] === ''){
+        continue
+      }
+
+      // for every winning number, you get additional copies of subsequent cards
+      if (winningNums.includes(myNums[j])){
+        cardWins++
+        if (cardCopies[cardNumber + cardWins]){
+          cardCopies[cardNumber + cardWins]++
+        } else {
+          cardCopies[cardNumber + cardWins] = 1
+        }
       }
     }
   }
-  total += cardPoints
 
 
-
-  console.log("winningNums: ", winningNums)
-  console.log("myNums: ", myNums)
-  console.log("total: ", total)
+  console.log("cardNumber: ", cardNumber)
+  console.log("cardCopies: ", cardCopies)
   console.log("****************")
 }
+
+totalCards = Object.values(cardCopies).reduce((a,b) => a + b)
+  console.log("totalCards: ", totalCards)
