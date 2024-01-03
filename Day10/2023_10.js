3979fbc6c9077d0a5c9ca504a88893bb2aae1288
Rachel Cheman <rachelcheman@gmail.com>
2024-01-03 10:46:54 -0500

    Day 10 part 2. Not currently working.

--- Day10/2023_10.js
+++ Day10/2023_10.js
@@ -5,6 +5,16 @@ const data = fs.readFileSync(path.resolve(__dirname, './2023_10_data.txt'), 'utf
 
 let rows = data.split("\n")
 
+
+class ArraySet extends Set {
+  add(arr) {
+    super.add(arr.toString());
+  }
+  has(arr) {
+    return super.has(arr.toString())
+  }
+}
+
 // find S (the animal's starting point)
 let startingIndex;
 for (let i = 0; i < rows.length; i++){
@@ -27,17 +37,20 @@ console.log(startingIndex)
  * "F" = south - east
  * "." = ground
  */
+  let loopCoors = new ArraySet([[startingIndex[0],startingIndex[1]]])
 findLoop(startingIndex, rows)
 
 function findLoop(startingIndex, rows){
   let loopLength = 0;
   let i = startingIndex[0]
   let j = startingIndex[1]
-  console.log(rows[i][j])
+
+  // console.log(rows[i][j])
   let currPipe = [[i],[j], "start"]
 
+
   // Check if we should go North
-  if (rows[i-1][j] === "|" || rows[i-1][j] === "7" || rows[i-1][j] === "F") {
+  if (i > 0 && (rows[i-1][j] === "|" || rows[i-1][j] === "7" || rows[i-1][j] === "F")) {
     i--
     currPipe = [i, j, rows[i][j], "N"]
   }
@@ -54,9 +67,10 @@ function findLoop(startingIndex, rows){
     i++
     currPipe = [i, j, rows[i][j], "S"]
   }
-  console.log("next pipe: ", currPipe )
+  // console.log("next pipe: ", currPipe )
 
   loopLength ++
+  loopCoors.add([i,j])
 
   while (rows[i][j] !== "S"){
     let currShape = currPipe[2]
@@ -111,16 +125,54 @@ function findLoop(startingIndex, rows){
       j++
       currPipe= [i,j,rows[i][j], "E"]
     }
+    loopCoors.add([i,j])
     loopLength++
     console.log(currPipe)
   }
 
   console.log("Final Loop length: ", loopLength)
 
+// find the length of the loop and divide that by 2
   console.log("Furthest steps: ", loopLength/2)
 
 }
 
-// function changeDirection()
 
-// find the length of the loop and divide that by 2
+// Find all the tiles that are contained by the loop
+
+// Starting inside the loop, keep moving across in the j direction until you find another loop end, all the tiles between are contained by the loop
+// Keep going until you find another loop edge, this is the start of a new loop containing section, continue until you find the closing loop for that section
+// Repeat
+
+findArea(loopCoors, rows)
+
+
+function findArea(loopCoors, rows){
+    console.log(loopCoors)
+  let edgesFound = 0
+  let tileCount = 0;
+
+  console.log(rows.length)
+
+  // TODO: This is broken and messed up and is giving inaccurate tile counts
+
+  for (let i = 0; i < rows.length; i++){
+    for (let j = 0; j < rows[i].length; j++){
+      if (loopCoors.has([i,j])){
+        edgesFound++
+        // In first line skip to below the edge of pipe so we are actually inside the pipe
+        if (edgesFound === 1){
+          i++;
+          j++
+        }
+      }
+      // if edges found is odd, that means we are inside the loop and should count tiles
+      else if (edgesFound % 2 !== 0){
+        tileCount++
+      }
+    }
+  }
+  console.log("tileCount: ", tileCount)
+}
+
+
